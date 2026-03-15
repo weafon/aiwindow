5434c4c1ee10fbeb0a3e2468d8f76c2a7ea39055
weafon tsao <>
2026-03-15 17:49:28 +0800

    allow to run from console

--- start_window.sh
+++ start_window.sh
@@ -6,11 +6,15 @@ export LC_ALL=en_US.UTF-8
 export PYTHONIOENCODING=utf-8
 export QT_QPA_PLATFORM=xcb
 export SDL_VIDEODRIVER=x11
+export PATH=/home/weafon/.espressif/python_env/idf5.5_py3.12_env/bin/:$PATH
+
+# 2. 指定 YouTube 載入工具（強烈建議使用 yt-dlp）
+# 如果你安裝的是 yt-dlp，請加上這一行：
+export MPV_YTDL_EXE="yt-dlp"
+source ~/.my.env
 cd ~/aiwindow
 # 1. 清理舊的 Socket
 rm -f /tmp/mpvsocket
-rm -f /home/weafon/.gemini/tmp/ec81af70508adebcba9dafaa5d302b0e8893f236fa9994eb1ef1ea1255a8bf83/mpv.log
-
 # 2. 啟動 mpv 並開啟 IPC 功能 (背景執行)
 echo "啟動窗景播放器 (idle mode)..."
 # 啟動 mpv 空閒模式，之後由 ai_window.py 隨機選取並下達 loadfile 指令
